b012e606d8675631f2a20ca1ac0ec5d56a6649ae
Prince0302 <115176604+Prince0302@users.noreply.github.com>
2023-02-27 23:26:45 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,21 @@
+//import java.util;
+import java.util.Scanner;
+
+public class record{
+    int reg;
+    char name;
+    int roll;
+    char address;
+    int phone;
+}
+public class vote {
+    
+public static void main(String [] args)
+{
+    System.out.println("Enter your reg-no:- ");
+    Scanner sc = new Scanner(System.in);
+
+    int reg = sc.nextInt();
+    System.out.println("student record"); 
+}
+}
